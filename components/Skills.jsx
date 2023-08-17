4e7fd9e9d5c87e55ddbadddae20734fab25a43d2
Pawel <pawel.bornikowski@gmail.com>
2023-08-17 22:07:26 +0200

    add icons to hard-skills section

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -2,6 +2,21 @@ import React from "react";
 import "@styles/skills.sass";
 import Arrows from "./arrows/Arrows";
 
+import {
+  CssIcon,
+  ReactIcon,
+  JsIcon,
+  HtmlIcon,
+  NodeIcon,
+  SassIcon,
+  VscodeIcon,
+  GitIcon,
+  GithubIcon,
+  NextIcon,
+  TsIcon,
+  ReduxIcon,
+} from "./images/icons/Icons";
+
 const Skills = () => {
   return (
     <section id="skills">
@@ -11,30 +26,34 @@ const Skills = () => {
           <div className="technologies ">
             <h3>Technologies</h3>
             <div className="icons-wrapper">
-             
+              <CssIcon />
+              <ReactIcon />
+              <JsIcon />
+              <HtmlIcon />
+              <NodeIcon />
+              <SassIcon />
+
+              {/* <div className="technologies-icons icons"></div>
                 <div className="technologies-icons icons"></div>
                 <div className="technologies-icons icons"></div>
                 <div className="technologies-icons icons"></div>
-                <div className="technologies-icons icons"></div>
-                <div className="technologies-icons icons"></div>
-              </div>
-             
-           
+                <div className="technologies-icons icons"></div> */}
+            </div>
           </div>
           <div className="tools ">
             <h3>Tools</h3>
             <div className="icons-wrapper">
-              <div className="tools-icons icons"></div>
-              <div className="tools-icons icons"></div>
-              <div className="tools-icons icons"></div>
+              <VscodeIcon />
+              <GitIcon />
+              <GithubIcon />
             </div>
           </div>
           <div className="learning ">
             <h3>In process of learning</h3>
             <div className="icons-wrapper">
-              <div className="learning-icons icons"></div>
-              <div className="learning-icons icons"></div>
-              <div className="learning-icons icons"></div>
+              <NextIcon />
+              <TsIcon />
+              <ReduxIcon />
             </div>
           </div>
         </div>
---
+++ components/images/icons/Icons.js
@@ -0,0 +1,54 @@
+import css_icon from '../../../assets/icons/technology/css-icon.png'
+import react_icon from '../../../assets/icons/technology/react-icon.png'
+import js_icon from '../../../assets/icons/technology/js-icon.png'
+import html_icon from '../../../assets/icons/technology/html-icon.png'
+import node_icon from '../../../assets/icons/technology/node-icon.png'
+import sass_icon from '../../../assets/icons/technology/sass-icon.png'
+
+import vscode_icon from '../../../assets/icons/tools/vscode-icon.png'
+import git_icon from '../../../assets/icons/tools/git-icon.png'
+import github_icon from '../../../assets/icons/tools/github-icon.png'
+
+import next_icon from '../../../assets/icons/learning/next-icon.png'
+import ts_icon from '../../../assets/icons/learning/ts-icon.png'
+import redux_icon from '../../../assets/icons/learning/redux-icon.png'
+import Image from 'next/image'
+
+export const CssIcon = () => {
+    return <Image src={css_icon} width={50} height={50}/>
+}
+export const ReactIcon = () => {
+    return <Image src={react_icon} width={50} height={50}/>
+}
+export const JsIcon = () => {
+    return <Image src={js_icon} width={50} height={50}/>
+}
+export const HtmlIcon = () => {
+    return <Image src={html_icon} width={50} height={50}/>
+}
+export const NodeIcon = () => {
+    return <Image src={node_icon} width={50} height={50}/>
+}
+export const SassIcon = () => {
+    return <Image src={sass_icon} width={50} height={50}/>
+}
+
+export const VscodeIcon = () => {
+    return <Image src={vscode_icon} width={50} height={50}/>
+}
+export const GitIcon = () => {
+    return <Image src={git_icon} width={50} height={50}/>
+}
+export const GithubIcon = () => {
+    return <Image src={github_icon} width={50} height={50}/>
+}
+
+export const NextIcon = () => {
+    return <Image src={next_icon} width={50} height={50}/>
+}
+export const TsIcon = () => {
+    return <Image src={ts_icon} width={50} height={50}/>
+}
+export const ReduxIcon = () => {
+    return <Image src={redux_icon} width={50} height={50}/>
+}
