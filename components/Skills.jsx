ef45bb9e0faf836d9265ce7c3bdb9242b6df0622
Pawel <pawel.bornikowski@gmail.com>
2023-08-18 16:50:34 +0200

    add slider navigation

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -3,14 +3,15 @@
 import "@styles/skills.sass";
 import Arrows from "./arrows/Arrows";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/hash-navigation";
 import { HardSkills, SoftSkills } from "./skills/SkillsComponent";
 //import 'swiper/swiper.min.css'
 
-import React, { useLayoutEffect, useState } from "react";
+import React, { useLayoutEffect, useState, useRef } from "react";
 
 const Skills = () => {
-  
   const windowWidthSize = () => {
     const [windowWidth, setWindowWidth] = useState(0);
     useLayoutEffect(() => {
@@ -34,12 +35,26 @@ const Skills = () => {
             spaceBetween={50}
             slidesPerView={1}
             className="slider-wrapper"
-            
             rows={1}
             fill={"row"}
+            modules={[Navigation]}
+            navigation={{
+              prevEl: ".swiper-button-prev",
+              nextEl: ".swiper-button-next",
+            }}
           >
-            <SwiperSlide className="left"><HardSkills/></SwiperSlide>
-            <SwiperSlide className="right"><SoftSkills/></SwiperSlide>
+            <SwiperSlide className="left">
+              <HardSkills />
+              <div className="swiper-button-next">
+                <div className="swiper-arrow s-a-next"></div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="right">
+              <div className="swiper-button-prev">
+                <div className="swiper-arrow s-a-prev"></div>
+              </div>
+              <SoftSkills />
+            </SwiperSlide>
           </Swiper>
         ) : (
           <>
@@ -98,8 +113,8 @@ const Skills = () => {
                 </div>
               </div>
             </div> */}
-            <HardSkills/>
-            <SoftSkills/>
+            <HardSkills />
+            <SoftSkills />
           </>
         )}
       </div>
