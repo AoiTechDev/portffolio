820d9061396fb8ce8b6f68b9f0e37fde17094be0
Pawel <pawel.bornikowski@gmail.com>
2023-08-18 18:19:57 +0200

    change arrow styles, add blured edges in nav

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -58,61 +58,7 @@ const Skills = () => {
           </Swiper>
         ) : (
           <>
-            {/* <div className="hard-skills">
-              <h2 className="hard-first-letter">Hard Skills</h2>
-              <div className="skills-wrapper">
-                <div className="technologies ">
-                  <h3>Technologies</h3>
-                  <div className="icons-wrapper">
-                    <CssIcon />
-                    <ReactIcon />
-                    <JsIcon />
-                    <HtmlIcon />
-                    <NodeIcon />
-                    <SassIcon />
-                  </div>
-                </div>
-                <div className="tools ">
-                  <h3>Tools</h3>
-                  <div className="icons-wrapper">
-                    <VscodeIcon />
-                    <GitIcon />
-                    <GithubIcon />
-                  </div>
-                </div>
-                <div className="learning ">
-                  <h3>In process of learning</h3>
-                  <div className="icons-wrapper">
-                    <NextIcon />
-                    <TsIcon />
-                    <ReduxIcon />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="soft-skills">
-              <h2 className="soft-first-letter">Soft Skills</h2>
-              <div className="skills-wrapper">
-                <div className="soft-skill">
-                  <TeamworkIcon />
-                  <h3>Teamwork</h3>
-                </div>
-                <div className="soft-skill">
-                  <h3>Communication</h3> <CommunicationIcon />
-                </div>
-                <div className="soft-skill">
-                  <SelfdisciplineIcon />
-                  <h3>Self-discipline</h3>
-                </div>
-                <div className="soft-skill">
-                  <h3>Problem-solving</h3> <ProblemIcon />
-                </div>
-                <div className="soft-skill">
-                  <CreativeIcon />
-                  <h3>Creative</h3>
-                </div>
-              </div>
-            </div> */}
+           
             <HardSkills />
             <SoftSkills />
           </>
