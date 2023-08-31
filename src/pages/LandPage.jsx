e0202dfded2d289f280d2652beb1f3b76d8d2fb1
Pawel <pawel.bornikowski@gmail.com>
2023-08-31 19:08:18 +0200

    clean up landing page code, fix elements positions

--- src/pages/LandPage.jsx
+++ src/pages/LandPage.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import Image from "next/image";
-import myphoto from "../../assets/images/myphoto.png";
 import "@styles/landpage.sass";
 import "animate.css";
-import Link from "next/link";
 import "@styles/arrows/arrows.sass";
-import Arrows from "../../components/visual_components/arrows/Arrows";
-import DevSVG from "@assets/icons/animation/dev_animation.svg";
 import frontImg from "@assets/images/nobg-frontimg.png";
 const Header = () => {
   return (
@@ -14,13 +10,11 @@ const Header = () => {
       <div className="landpage-content-wrapper">
         <div className="landpage-headline">
           <div className="my-name">
-            <h1>Hi, my name is</h1>
-            <h1>Paweł Bornikowski</h1>
+            <h1>Hi, Im Paweł</h1>
+            <h1>Bornikowski</h1>
           </div>
 
           <h3>Front-end Developer</h3>
-
-          {/* <div className="first-blob"></div> */}
         </div>
         <Image src={frontImg} className="front-img" />
       </div>
