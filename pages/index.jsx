eba7dfbe6495a7dfcb52e5e4b4cdd377ca564d14
Pawel <pawel.bornikowski@gmail.com>
2023-09-13 18:15:22 +0200

    delete front-page buttons, add neon effect to letters F and e

--- pages/index.jsx
+++ pages/index.jsx
@@ -26,14 +26,39 @@ const LandPage = () => {
       <section id="landpage">
         <div className="container">
           <div className="my-name">
-            <p>Hi, my name is</p>
-            <h1>Paweł</h1>
+            {/* <p>Hi, my name is</p> */}
+            <h1>
+              Hi, I am <span>Paweł Bornikowski</span> a{" "}
+              <span className="glowing-txt">
+                <span
+                  className="faulty-letter"
+                  style={{
+                    "--glow-color": "hsl(186 100% 69%)",
+                  }}
+                >
+                  F
+                </span>
+                ront-
+                <span
+                  className="faulty-letter"
+                  style={{
+                    "--glow-color": "hsl(297.79, 100%, 52.16%)",
+                  }}
+                >
+                  e
+                </span>
+                nd 
+              </span> 
+              <span>&nbsp;Developer</span>
+            </h1>
+            {/* <h1>Paweł</h1>
             <h1>Bornikowski</h1>
-            <h3 className="frontend-text">Front-end Developer</h3>
+            <h3 className="frontend-text">Front-end Developer</h3> */}
             <p className="front-note">
-             Aspiring pixel perfect craftsman honing my JS, React & CSS skills  to build interfaces that delight
+              Aspiring pixel perfect craftsman honing my JS, React & CSS skills
+              to build interfaces that delight.
             </p>
-            <div className="btns-container">
+            {/* <div className="btns-container">
               <Link href="/About">
                 <button className="glowing-btn" style={{'--glow-color':'hsl(186 100% 69%)'}}>
                   <span className="glowing-txt">DI<span className="faulty-letter">V</span>E IN</span>
@@ -48,12 +73,19 @@ const LandPage = () => {
                  
                   </button>
               </Link>
-            </div>
+            </div> */}
           </div>
         </div>
         <div className="socials">
-          <Link href="https://github.com/BorPawel" target="_blank"><BsGithub/></Link>
-          <Link href="https://www.linkedin.com/in/pbornikowski/" target="_blank"><BsLinkedin/></Link>
+          <Link href="https://github.com/BorPawel" target="_blank">
+            <BsGithub />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/pbornikowski/"
+            target="_blank"
+          >
+            <BsLinkedin />
+          </Link>
         </div>
       </section>
     </Page>
