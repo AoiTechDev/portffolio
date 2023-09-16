2114413ecf1a6182cb6a53a9f72f3bd1c5e85acc
Pawel <pawel.bornikowski@gmail.com>
2023-09-16 02:55:09 +0200

    nav menu animations v1

--- components/menu/Menu.js
+++ components/menu/Menu.js
@@ -14,22 +14,50 @@ const Menu = ({ open, menuOpenHandler }) => {
       bottom: 0,
       ease: "power1.InOut",
 
-      duration: 0.5,
-    }).to(".nav-container", {
-      opacity: 1,
-      ease: "power1.InOut",
-      duration: 1,
-    });
-    
-    // tl.staggerFrom(
-    //   ".menu > div",
-    //   0.8,
-    //   { y: 100, opacity: 0, ease: "power1.InOut" },
-    //   "0.1",
-    //   "-=0.4"
-    // );
+      duration: 0.3,
+    })
+      .to(".menu-left", {
+        left: 0,
+        ease: "power1.InOut",
 
-    // tl.staggerFrom(
+        duration: 0.3,
+      })
+      .to(".menu-right .menu-right-up", {
+        right: 0,
+        ease: "power1.InOut",
+
+        duration: 0.3,
+      })
+      .to(".menu-right .menu-right-down", {
+        right: 0,
+        ease: "power1.InOut",
+
+        duration: 0.3,
+      })
+      .to(".menu-close-container", {
+        top: "80%",
+        ease: "power1.InOut",
+        duration: 0.4,
+      })
+      .to(".menu-close-container .menu-line", {
+        height: 400,
+        ease: "power1.InOut",
+        duration: 0.4,
+      })
+      .to(".menu-close-container .menu-close", {
+        y: -100,
+        ease: "power1.InOut",
+        duration: 0.4,
+      })
+      .staggerFrom(
+        ".menu > div",
+        0.8,
+        { y: 100, opacity: 0, ease: "power1.InOut" },
+        "0.1",
+        "-=0.4"
+      );
+
+    // .staggerFrom(
     //   ".socials",
     //   0.8,
     //   { y: 100, opacity: 0, ease: "power1.InOut" },
@@ -54,30 +82,43 @@ const Menu = ({ open, menuOpenHandler }) => {
 
   return (
     <div class="nav-container">
-      <div class="menu-close">close</div>
+      <div className="menu-close-container">
+        <div class="menu-close">
+          <span></span>
+          <span></span>
+        </div>
+        <span className="menu-line"></span>
+      </div>
 
-      <nav class="menu">
-        <div class="menu_item">
-          <Link class="menu_item-link" href="/About">
-            About me
-          </Link>
-        </div>
-        <div class="menu_item">
-          <Link class="menu_item-link" href="/Projects">
-            Projects
-          </Link>
-        </div>
-        <div class="menu_item">
-          <Link class="menu_item-link" href="/Skills">
-            Skills
-          </Link>
-        </div>
-        <div class="menu_item">
-          <Link class="menu_item-link" href="/Contact">
-            Contact
-          </Link>
-        </div>
-      </nav>
+      <div className="menu-left">
+        <nav class="menu">
+          <div class="menu_item">
+            <Link class="menu_item-link" href="/About">
+              About me
+            </Link>
+          </div>
+          <div class="menu_item">
+            <Link class="menu_item-link" href="/Projects">
+              Projects
+            </Link>
+          </div>
+          <div class="menu_item">
+            <Link class="menu_item-link" href="/Skills">
+              Skills
+            </Link>
+          </div>
+          <div class="menu_item">
+            <Link class="menu_item-link" href="/Contact">
+              Contact
+            </Link>
+          </div>
+        </nav>
+      </div>
+
+      <div className="menu-right">
+        <div className="menu-right-up"></div>
+        <div className="menu-right-down"></div>
+      </div>
     </div>
   );
 };
